d9f3fe286dfa3276a04257fcd69da5079b8613d4
Yanzuo Liu <liuyanzuo@zhiweidata.com>
2019-01-10 14:22:36 +0800

    Ver 1.1.0 Add automatic work and automatic training function
    Add automatic work and automatic training function。

--- js/content-script.js
+++ js/content-script.js
@@ -13,44 +13,73 @@ $(document).ready(function () {
 function start() {
     let href = window.location.href;
     console.log(href);
+    if (document.getElementById("loginContainer")) {
+        console.log("should login");
+        return;
+    }
     console.log("plugin starting");
     let limitReset = $("#limitReset").text();
     setTimeout(function () {
         window.location.reload();//刷新当前页面.
     }, parseInt(limitReset) * 1000);
+    setInterval(function () {
+        console.log("Count down:" + $("#minutesLimit").text() + $("#secondsLimit").text());
+    }, 10000);
     console.log("Limit Reset:" + limitReset);
     let actualHealth = $("#actualHealth").text();
     console.log("Actual Health:" + actualHealth);
-    if (href.split("/")[3].split(".html")[0] === "battles") {
+    let location = href.split("/")[3].split(".html")[0];
+    console.log("Page location:" + location);
+
+    if (location === "battles") {
         console.log("battles");
-		for(let a of $("#battlesTable").children("tbody").children("tr").find("a")){
-			if($(a).text().indexOf("新手战场")!==-1){
-				window.location.href = "http://omega.e-sim.org/battle.html?id="+$(a).attr("href").split("id=")[1]
-			}
-		}
-    } else if (href.split("/")[3].split(".html")[0] === "battle") {
-        let attack = setTimeout(function () {
-            if (actualHealth !== "0.0") {
-                if (document.getElementById("fightButtonBerserk1") !== null) {
-                    document.getElementById("fightButtonBerserk1").click();
-                    window.location.reload();
-                }
-                if (document.getElementById("fightagainbutton") !== null) {
-                    document.getElementById("fightagainbutton").click();
-                }
-                actualHealth = $("#actualHealth").text();
-                console.log("Attack! Actral health:" + actualHealth)
-            } else {
-                window.clearInterval(attack);
+        for (let a of $("#battlesTable").children("tbody").children("tr").find("a")) {
+            if ($(a).text().indexOf("新手战场") !== -1) {
+                window.location.href = "http://omega.e-sim.org/battle.html?id=" + $(a).attr("href").split("id=")[1]
+                return ;
             }
-        }, 5000);
-        setInterval(function () {
-            console.log("Count down:" + $("#minutesLimit").text() + $("#secondsLimit").text())
-            let battleText = $(".foundation-radius.fightContainer.foundation-base-font").text();
-            if (battleText.indexOf("这轮的胜利方：") !== -1) {
-                window.location.href = "http://omega.e-sim.org/battles.html?countryId=309"
+        }
+    } else if (location === "battle") {
+        if (actualHealth !== "0.0") {
+            if (document.getElementById("fightButtonBerserk1") !== null) {
+                document.getElementById("fightButtonBerserk1").click();
+                window.location.reload();
+                return ;
             }
-        }, 2000);
+            actualHealth = $("#actualHealth").text();
+            console.log("Attack! Actral health:" + actualHealth)
+        }
+        let battleText = $(".foundation-radius.fightContainer.foundation-base-font").text();
+        if (battleText.indexOf("这轮的胜利方：") !== -1) {
+            window.location.href = "http://omega.e-sim.org/battles.html?countryId=309"
+            return ;
+        }
+    } else if (location === "work") {
+        console.log("Work work");
+        if (document.getElementById("workButton") != null) {
+            document.getElementById("workButton").click();
+        }
+        window.location.href = "http://omega.e-sim.org/index.html";
+        return ;
+    } else if (location === "train") {
+        console.log("Train train");
+        if (document.getElementById("trainButton") != null) {
+            document.getElementById("trainButton").click();
+        }
+        window.location.href = "http://omega.e-sim.org/index.html";
+        return ;
+    }
+    if (document.getElementById("taskButtonTrain") != null) {
+        window.location.href = "http://omega.e-sim.org/train.html";
+        return ;
+    }
+    if (document.getElementById("taskButtonWork") != null) {
+        window.location.href = "http://omega.e-sim.org/work.html";
+        return ;
+    }
+    if (document.getElementById("taskButtonFight") != null) {
+        window.location.href = "http://omega.e-sim.org/battles.html?countryId=309";
+        return ;
     }
 
 }
